515fcdef946cd414d6058028487d9749136a1c4f
shiv <shivedrasrivastava490@gmail.com>
2022-04-10 00:59:45 +0530

    UI done except img

---
+++ src/main/Main.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./main.css";
+import planetList from "../planetList"
+
+// const planetList = [
+//   "earth",
+//   "jupiter",
+//   "mars",
+//   "mercury",
+//   "moon",
+//   "neptune",
+//   "pluto",
+//   "saturn",
+//   "uranus",
+//   "venus",
+// ];
+
+const Main = () => {
+  const [weight, setWeight] = useState("select planet");
+  const [planet, setPlanet] = useState("select planet");
+  
+
+  const handlePlanet = (e) => {
+    setPlanet(e.target.value);
+  };
+
+  const handleWeight = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const handleButton = () => {};
+
+  return (
+    <div className="main-background">
+      <div className="heading">Calculate weight of an object on planet.</div>
+      <div className="inputContainer">
+        <input
+          type="number"
+          name="weight"
+          className="weight"
+          value={weight}
+          onChange={handleWeight}
+          placeholder="enter weight"
+        />
+        {/* {console.log(planetList)} */}
+        <select
+          value={planet}
+          name="planet"
+          className="planet"
+          onChange={handlePlanet}
+        >
+          {planetList.map((curElem) => {
+            return <option value={curElem.planetName} key={curElem.id}>{curElem.planetName}</option>;
+          })}
+        </select>
+        <button className="btn" onClick={handleButton}>
+          Calculate weight
+        </button>
+      </div>
+
+      {/* <div className="invalid-card">
+        <div className="invalid">weight is required
+        </div>
+      </div> */}
+
+      <div className="valid-card">
+        <div className="picture">
+          <img src='../images/moon.png' alt="img"/>
+        </div>
+        <div className="main-result-container">
+            <div className="result-sq">
+              <div className="sq-content">The weight of the object on Pluto is</div>
+              <div className="circle-center">
+              <div className="circle">7.00
+              </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/planetList.js
@@ -0,0 +1,14 @@
+const planetList=[
+    {id:1,planetName:"earth",path:'../images/earth.png',factor:9.8},
+    {id:2,planetName:"jupiter",path:'../images/jupiter.png',factor:8.8},
+    {id:3,planetName:"mars",path:'../images/mars.png',factor:6.8},
+    {id:4,planetName:"mercury",path:'../images/mercury.png',factor:7.8},
+    {id:5,planetName:"moon",path:'../images/moon.png',factor:11},
+    {id:6,planetName:"neptune",path:'../images/neptune.png',factor:9.0},
+    {id:7,planetName:"pluto",path:'../images/pluto.png',factor:9.2},
+    {id:8,planetName:"saturn",path:'../images/saturn.png',factor:9.8},
+    {id:9,planetName:"uranus",path:'../images/uranus.png',factor:5.8},
+    {id:10,planetName:"venus",path:'../images/venus.png',factor:4}
+]
+
+export default planetList
